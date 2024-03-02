616aeb06fcac67cc9abbbf2169d618d363fb212d
Thelford Williams <tdwilliamsiv@gmail.com>
2024-03-02 15:59:37 -0800

    flipping mips bytes correctly

--- instructionguessing/src/main.rs
+++ instructionguessing/src/main.rs
@@ -1,5 +1,17 @@
 use std::env;
 use u32;
+
+/*
+81 18 e4 b3
+18 81 b3 e4
+*/
+fn mips_mix_around(num: u32) -> u32 {
+    ((num & 0xff000000) >> 8)
+        + ((num & 0x00ff0000) << 8)
+        + ((num & 0x0000ff00) >> 8)
+        + ((num & 0xff) << 8)
+}
+
 fn clean_string(input: &str) -> Result<u32, String> {
     if input.len() % 2 != 0 {
         return Err(String::from("Should be even length string"));
@@ -10,7 +22,8 @@ fn clean_string(input: &str) -> Result<u32, String> {
         String::from(input)
     };
     match u32::from_str_radix(&input, 16) {
-        Ok(i) => Ok(i),
+        // for mips16e the instruction needs to be mixed around
+        Ok(i) => Ok(mips_mix_around(i)),
         Err(e) => Err(format!("failed to parse {:?}", &e)),
     }
 }
